80e2da358163a7208e722ed39b739b1a95e2bf50
imlam <burninglam@qq.com>
2023-09-21 13:25:35 +0800

    init

---
+++ src/main/java/cn/burningdan/SpringbootActionDemoApplication.java
@@ -0,0 +1,13 @@
+package cn.burningdan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootActionDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootActionDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/burningdan/SpringbootActionDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.burningdan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootActionDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
